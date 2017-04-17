39b8e065beccc581a52e03e078dd7438589f8b48
silence717 <yangfang2028@126.com>
2017-04-17 19:25:43 +0800

    test thunk

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -6,6 +6,9 @@
 import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as type from './actionTypes';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import cofigureMockStore from 'redux-mock-store';
 
 // Test a sync action
 describe('Course Actions', () => {
@@ -25,4 +28,31 @@ describe('Course Actions', () => {
       expect(action).toEqual(expectedAction);
     });
   });
+});
+
+const middleware = [thunk];
+const mockStore = cofigureMockStore(middleware);
+
+describe('Async Actions', () => {
+  afterEach( ()=> {
+    nock.cleanAll();
+  });
+  
+  it('should create BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading courses', (done) => {
+    
+    const expectedActions = [
+      {type: type.BEGIN_AJAX_CALL},
+      {type: type.LOAD_COURSES_SUCCESS, body: {courses: [{id: 'clean-code', title: 'Clean Code'}]}}
+    ];
+    
+    const store = mockStore({course: []}, expectedActions);
+    store.dispatch(courseActions.loadCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(type.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(type.LOAD_COURSES_SUCCESS);
+      done();
+    });
+    
+  });
+  
 });
